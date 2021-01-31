af566d12ebec40f7e175ec75bf1adff9a4a74ec7
Alexis Lo <alo@lacreativeria.com>
2021-01-30 21:02:37 -0600

    tarea

---
+++ script.py
@@ -0,0 +1,37 @@
+############# TIPOS DE DATOS:
+# int es el tipo de dato igual a numero entero 22
+# float es igual a 22.222
+# boolean es igual a verdadero o falso, true o false
+# string es una cadena de texto "Hola mundo 22 + 22".
+
+# Júpiter = 2.55 Venus = 0.87 Urano = 0.99 Marte = 0.38 Mercurio = 0.39 Saturno = 0.93 Neptuno = 1.38 Tierra = 1 Luna = 0.17
+
+####################################################
+############### EJEMPLO ALAMBRADO
+# pesoDeKeylor = 80
+
+# pesoTotalEnLaLuna = pesoDeKeylor * 0.17
+
+# pesoDeKeylorEnLaLuna = "Usted pesa: " + str(pesoTotalEnLaLuna) + "kg en la luna"
+
+# print(pesoDeKeylorEnLaLuna)
+
+
+################ PESO EN LA LUNA: 
+# pesoDelUsuario = input("Por favor ingresa tu peso: ")
+# pesoTotalEnLaLuna = pesoDelUsuario * 0.17
+
+# pesoDelUsuarioEnLaLuna = "Usted pesa: " + str(pesoTotalEnLaLuna) + "kg en la luna"
+# print(pesoDelUsuarioEnLaLuna)
+
+################ PESO EN MARTE: 
+
+pesoDelUsuario = input("Por favor ingresa tu peso: ")
+pesoTotalEnLaLuna = pesoDelUsuario * 0.17
+
+pesoDelUsuarioEnLaLuna = "Usted pesa: " + str(pesoTotalEnLaLuna) + "kg en la luna"
+print(pesoDelUsuarioEnLaLuna)
+
+################ PESO EN MARTE: 
+
+
